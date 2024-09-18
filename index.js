0dac16d0843a1e2fbccf9e3413119855a7ced7f4
Mansoor <shaikmansoor1200@gmail.com>
2024-09-18 07:50:28 +0530

    Setup server and client build

--- index.js
+++ index.js
@@ -4022,7 +4022,17 @@ if(process.env.NODE_ENV === 'production'){
     res.send('<h1>API is running...</h1>');
   });
 }
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
 
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//   });
+// } else {
+//   app.get('/', (req, res) => {
+//     res.send('<h1>API is running...</h1>');
+//   });
+// }
 
 
 
