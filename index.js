ee9d112a04adc4b7d2f3d64fd6148b0a09805f76
Mansoor <shaikmansoor1200@gmail.com>
2024-09-20 09:07:40 +0530

    trying to resolve the refresh issue

--- index.js
+++ index.js
@@ -2008,9 +2008,9 @@ app.post('/run-code', async (req, res) => {
 // }
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 } else {
   app.get('/', (req, res) => {
