6f47a93859924df8fc09fab4522594e6a54e86ab
Mansoor <shaikmansoor1200@gmail.com>
2024-10-19 18:40:49 +0530

    candidate in index.js

--- index.js
+++ index.js
@@ -120,20 +120,23 @@ const broadcastImageData = async (type, id) => {
 };
 
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Add this route to handle the root URL
+// // Add this route to handle the root URL
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+
+// // Handle any other requests and serve the React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+
+// Define a route for the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.send('Welcome to the Interview App!');
 });
-
-// Handle any other requests and serve the React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
-
-
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
