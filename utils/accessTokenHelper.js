32404abbbe5e5cacc3001b6b698a3b155457e059
Mansoor <shaikmansoor1200@gmail.com>
2024-10-19 18:26:05 +0530

    candidate in index.js

--- utils/accessTokenHelper.js
+++ utils/accessTokenHelper.js
@@ -1,4 +1,4 @@
-const ConnectedApp = require('../models/ConnectedApp');
+const ConnectedApp = require('../models/ConnectedApp1.js');
 const generateRandomString = require('./generateRandomString');
 const formatDate = (date) => date.toISOString().split('T')[0];
 
